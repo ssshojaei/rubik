c25d0218caad9224c96e2d97d0ac53ccb13c24f2
ssshojaei <ordinarysaleh@pm.me>
2020-12-24 22:01:35 +0330

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(5);
+  const styles = {
+    gridTemplateColumns: `repeat(${count}, 1fr)`,
+    gridTemplateRows: `repeat(${count}, 1fr)`,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="rotate-x">
+        <div className="rotate-y">
+          <div className="rotate-z">
+            <div className="cube">
+              <div style={styles} className="panel panel-top">
+                {[...new Array(count * count)].map(() => (
+                  <div></div>
+                ))}
+              </div>
+              <div style={styles} className="panel panel-bottom">
+                {[...new Array(count * count)].map(() => (
+                  <div></div>
+                ))}
+              </div>
+              <div style={styles} className="panel panel-left">
+                {[...new Array(count * count)].map(() => (
+                  <div></div>
+                ))}
+              </div>
+              <div style={styles} className="panel panel-right">
+                {[...new Array(count * count)].map(() => (
+                  <div></div>
+                ))}
+              </div>
+              <div style={styles} className="panel panel-back">
+                {[...new Array(count * count)].map(() => (
+                  <div></div>
+                ))}
+              </div>
+              <div style={styles} className="panel panel-front">
+                {[...new Array(count * count)].map(() => (
+                  <div></div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <input
+        style={{
+          position: "fixed",
+          bottom: 10,
+        }}
+        type="range"
+        value={count}
+        onChange={(e) => setCount(e.target.value)}
+        min={2}
+        max={10}
+      />
+    </>
   );
-}
+};
 
 export default App;
